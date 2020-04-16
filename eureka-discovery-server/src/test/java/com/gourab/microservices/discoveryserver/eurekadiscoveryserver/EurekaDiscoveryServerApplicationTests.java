0ae551e902cb3d00d395c48694ed5bc9d788aca2
Gourab Mukherjee <gourab.mukherjee3@gmail.com>
2020-04-16 22:08:28 +0530

    Eureka Discovery Service

---
+++ eureka-discovery-server/src/test/java/com/gourab/microservices/discoveryserver/eurekadiscoveryserver/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourab.microservices.discoveryserver.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
